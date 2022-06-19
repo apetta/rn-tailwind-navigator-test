18b485f5ba33873505f7428dd3acf9a7e7bbd258
Akshay Petta <akshaypetta@gmail.com>
2022-06-19 12:22:33 +0100

    removing unused imports

--- App.js
+++ App.js
@@ -1,8 +1,4 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StyleSheet, Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
-import HomeScreen from "./screens/HomeScreen";
 import StackNavigator from "./StackNavigator";
 
 export default function App() {
